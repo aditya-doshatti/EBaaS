528bd2cc148a87f05c00d083eefde4be1463a69c
Darshil Kapadia <darshilpk3@gmail.com>
2020-05-03 03:41:05 -0700

    updated .sql file for local use.

--- ref/ebaas.sql
+++ ref/ebaas.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2020 at 12:07 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 5.6.37
+-- Generation Time: Apr 30, 2020 at 10:05 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,8 +33,18 @@ CREATE TABLE `application` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `server` varchar(255) DEFAULT NULL,
-  `launched` tinyint(1) NOT NULL DEFAULT '0'
+  `launched` tinyint(1) NOT NULL DEFAULT 0,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp(),
+  `launched_on` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `application`
+--
+
+INSERT INTO `application` (`id`, `userid`, `name`, `description`, `server`, `launched`, `created_on`, `launched_on`) VALUES
+(8, 7, 'test', NULL, NULL, 0, '2020-04-26 22:50:48', NULL),
+(9, 8, 'TestApplication', NULL, NULL, 1, '2020-04-26 22:50:48', '2020-04-27 00:18:01');
 
 -- --------------------------------------------------------
 
@@ -51,6 +60,14 @@ CREATE TABLE `user` (
   `country` varchar(255) DEFAULT NULL,
   `organisation` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `emailid`, `password`, `name`, `country`, `organisation`) VALUES
+(7, 'xyz@getnada.com', '$2b$12$UcJqsloQAG6P5iT1cDstmOvSdRnhBOOeZ3VpZa/fBTjzpOb9.jOqa', 'xyz', NULL, NULL),
+(8, 'testacc@gmail.com', '$2b$12$o0v1vf9LW7yoQNeJkvOqh.Pi1FAq8rGb.r92hSHV9TSdMr.eHUhgS', 'Test Account', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -68,6 +85,17 @@ CREATE TABLE `user_databases` (
   `password` varchar(255) NOT NULL,
   `dbname` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_databases`
+--
+
+INSERT INTO `user_databases` (`id`, `userid`, `applicationid`, `connectionname`, `hostname`, `username`, `password`, `dbname`) VALUES
+(8, 7, 8, 'myFirstConnection', 'localhost', 'root', 'root', 'ebaas'),
+(15, 8, 9, 'demoConnection', 'localhost', 'root', 'root', 'demoDb'),
+(16, 8, 9, 'demodb1', 'localhost', 'root', 'root', 'demodb1'),
+(17, 8, 9, 'ebaasConnection', 'localhost', 'root', 'root', 'xyz'),
+(18, 8, 9, 'ExcelDatabase', 'localhost', 'root', 'root', 'ExcelDatabase');
 
 --
 -- Indexes for dumped tables
@@ -104,19 +132,19 @@ ALTER TABLE `user_databases`
 -- AUTO_INCREMENT for table `application`
 --
 ALTER TABLE `application`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user_databases`
 --
 ALTER TABLE `user_databases`
-  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
